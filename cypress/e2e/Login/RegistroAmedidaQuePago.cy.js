34ae03320406a92c5f08ce297323aa57981d52b3
Fernando <ferzuriaga@gmail.com>
2024-09-06 01:01:12 -0300

    Comprobando suma de precios con el total

--- cypress/e2e/Login/RegistroAmedidaQuePago.cy.js
+++ cypress/e2e/Login/RegistroAmedidaQuePago.cy.js
@@ -15,26 +15,71 @@ describe("Registro a medida que pago", () => {
         cy.Login("pedrillo@gmail.com", "holapaola")
 
         //const indicesProductos = [3,4,5]
-         //const productos = cy.get(".product-image-wrapper")
-         
-         cy.get(".product-image-wrapper").each((el,index) =>{
+        //const productos = cy.get(".product-image-wrapper")
+
+        cy.get(".product-image-wrapper").each((el, index) => {
             //cy.log(el, index)
-            if( [3,4,5].includes(index) ){
+            if ([3, 4, 5].includes(index)) {         //el include busca lo elementos
                 cy.wrap(el).find(".productinfo a").click()
                 cy.contains("Continue Shopping").click()
 
             }
 
 
-         })
+        })
 
-         
+        cy.contains("Cart").click()
+        cy.get(".btn-default").should("have.text", "Proceed To Checkout")
+        cy.get(".col-sm-6 .btn-default").first().click()
+
+        const precios = []
+
+        cy.get(".cart_total_price").each((el) => {
+            const texto = el.text().replace("Rs.", "");
+            const textoValor = parseFloat(texto)
+            precios.push(textoValor)
+        }).then(() => {
+
+            cy.log(precios)
+            cy.get("tbody tr:last-child td:last-child").then((precioTotal) => {
+                //cy.log(precioTotal.text())
+
+                precioTotal= precioTotal.text().replace("Rs.", "")
+                precioTotal = parseFloat(precioTotal)
 
 
+
+
+
+                 let sumaTotalPrecios = 0
+
+                 sumaTotalPrecios =precios.reduce((acumulador, valorActual) => acumulador + valorActual, 0)
+
+                  // se quita la suma del precio total que se agrego al arreglo precio, 
+                  // ejemplo mi producto a = 200 ,  b = 300, c = 100 total 500 .. entonces aca se sumaron todos los valores desde el 200 al 500
+                  //entonces agarro y a ese precio total lo resto..
+                 sumaTotalPrecios = sumaTotalPrecios - precioTotal 
+
+                
+
+                 //lo que esta comentado es otra manera de hacerlo
+                    // let sumaTotalPrecios = 0                                             
+                    // precios.forEach((itemPrecio) => {
+                    //     sumaTotalPrecios += itemPrecio
+                    // })
+
+                    // sumaTotalPrecios = sumaTotalPrecios - precioTotal         
+
+                expect(precioTotal).to.equal(sumaTotalPrecios)
+            })
+        })
         
-         cy.contains("Cart").click()
-         cy.get(".btn-default").should("have.text", "Proceed To Checkout")
-        // cy.get(".col-sm-6 .btn-default").first().click()
+
+
+
+
+
+
 
         // cy.get(".address_firstname").should("contain.text", "Mr. Albafica Piscis")
         // cy.get(".address_address1").should("contain.text", "CasaDePiscis")
